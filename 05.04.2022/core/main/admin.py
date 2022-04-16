2dc854efe4a446efa9ea68afe59ec77fc3d7b97a
Maxim <honse321@gmail.com>
2022-04-16 13:26:09 +0300

    iop

--- 05.04.2022/core/main/admin.py
+++ 05.04.2022/core/main/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from main.models import Customer, book, Students
 
-# Register your models here.
+
+
+@admin.register( book)
+class Boooks(admin.ModelAdmin):
+    pass
+
+@admin.register(Students)
+class Stud(admin.ModelAdmin):
+    fields = ('firstname', 'books')
+    list_display = ('firstname', 'books')
+    list_filter = ('firstname',)
+
+@admin.register(Customer)
+class Cust(admin.ModelAdmin):
+    pass
+
--- 05.04.2022/core/main/models.py
+++ 05.04.2022/core/main/models.py
@@ -1,4 +1,19 @@
+from pyexpat import model
 from django.db import models
+
+class book(models.Model):
+    title = models.CharField(max_length=255)
+    
+    def __str__(self) -> str:
+        return f'{self.title}'
+
+
+class Students(models.Model):
+    firstname = models.CharField(max_length=255)
+    books = models.OneToOneField(book, null=True, on_delete=models.SET_NULL)
+
+    def __str__(self) -> str:
+        return f'{self.firstname}'
 
 class Customer(models.Model):
     firstname = models.CharField(max_length=255)
